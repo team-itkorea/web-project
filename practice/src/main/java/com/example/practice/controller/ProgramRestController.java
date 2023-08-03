79c2fd1c984a2b1d8003f6e22d1c22c54498ff78
sanghyun27 <ajvlzla0207@gmail.com>
2023-08-03 19:02:30 +0900

    no message

--- practice/src/main/java/com/example/practice/controller/ProgramRestController.java
+++ practice/src/main/java/com/example/practice/controller/ProgramRestController.java
@@ -6,13 +6,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.practice.dto.ProgramListRespDto;
+import com.example.practice.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 public class ProgramRestController {
+	
+	private final UserService userService;
+	
 	@GetMapping("/program/{theme}")
 	public ResponseEntity<?> getProgramList(@PathVariable String theme) {
 		
 		ProgramListRespDto listRespDto = null;
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
